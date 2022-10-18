0629379a92766409aa83bf05ee4b74f2192f59ff
Johnny Peter <johnnypeter2002@gmail.com>
2022-10-18 20:54:49 +0530

    NINGG-6776: add standalone executeThreeDS method

---
+++ payment-sdk-core/src/main/java/payment/sdk/android/core/PaymentResponse.kt
@@ -0,0 +1,77 @@
+package payment.sdk.android.core
+
+import com.google.gson.annotations.SerializedName
+
+
+class PaymentResponse {
+    @SerializedName(value = "_links")
+    var links: Links? = null
+
+    var amount: Amount? = null
+    var outletId: String? = null
+    var reference: String? = null
+    var orderReference: String? = null
+    var paymentMethods: PaymentMethods? = null
+    var authenticationCode: String? = null
+
+    @SerializedName(value = "3ds2")
+    var threeDSTwo: ThreeDSTwo? = null
+
+    @SerializedName(value = "_embedded")
+    var embedded: Embedded? = null
+
+    // Other classes
+    class Amount {
+        var currencyCode: String? = null
+        var value: Int? = 0
+    }
+
+    class Links {
+        @SerializedName(value = "payment-authorization")
+        var paymentAuthorizationUrl: Href? = null
+
+        @SerializedName(value = "payment")
+        var paymentUrl: Href? = null
+
+        @SerializedName(value = "cnp:3ds2-challenge-response")
+        var threeDSChallengeResponseUrl: Href? = null
+
+        @SerializedName(value = "cnp:3ds2-authentication")
+        var threeDSAuthenticationsUrl: Href? = null
+    }
+
+    class Embedded {
+        lateinit var payment: Array<Payment>
+    }
+
+    class Payment {
+        @SerializedName(value = "_links")
+        var links: PaymentLinks? = null
+    }
+
+    class PaymentLinks {
+        @SerializedName(value = "payment:samsung_pay")
+        var samsungPayLink: Href? = null
+    }
+
+    class Href {
+        var href: String? = null
+    }
+
+    class PaymentMethods {
+        var card: List<String>? = null
+        var wallet: Array<String>? = null
+    }
+
+    class ThreeDSTwo {
+        var messageVersion: String? = null
+        var threeDSMethodURL: String? = null
+        var threeDSServerTransID: String? = null
+        var directoryServerID: String? = null
+        var transStatus: String? = null
+        var acsTransID: String? = null
+        var acsReferenceNumber: String? = null
+        var acsSignedContent: String? = null
+
+    }
+}
--- payment-sdk-core/src/main/java/payment/sdk/android/core/TransactionService.kt
+++ payment-sdk-core/src/main/java/payment/sdk/android/core/TransactionService.kt
@@ -2,6 +2,7 @@ package payment.sdk.android.core
 
 interface TransactionService {
 //    fun getOrder(paymentLink: String, paymentAuthorizationLink: String): Order
+    fun getAuthTokenFromCode(url: String, code: String, success: (List<String>, String) -> Unit, error: (Exception) -> Unit)
     fun authorizePayment(order: Order, onResponse: (authTokens: HashMap<String, String>?, error: Exception?) -> Unit)
     fun acceptSamsungPay(encryptedObject: String,
                          samsungPayLink: String,
