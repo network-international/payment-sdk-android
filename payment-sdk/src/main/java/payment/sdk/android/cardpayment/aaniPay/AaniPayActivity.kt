804618e34a6468ae0e309616777c5cdcbe2296f6
Gautam Chibde <gautamchibde@gmail.com>
2024-06-26 10:15:20 +0530

    NINGG-10824 - Added UI for aani pay

---
+++ payment-sdk/src/main/java/payment/sdk/android/cardpayment/aaniPay/AaniPayActivity.kt
@@ -0,0 +1,93 @@
+package payment.sdk.android.cardpayment.aaniPay
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import payment.sdk.android.cardpayment.CardPaymentData
+import payment.sdk.android.cardpayment.aaniPay.model.AaniPayActivityArgs
+import payment.sdk.android.cardpayment.visaInstalments.model.VisaInstalmentActivityArgs
+import payment.sdk.android.sdk.R
+
+class AaniPayActivity : AppCompatActivity() {
+
+    private val inputArgs: AaniPayActivityArgs? by lazy {
+        AaniPayActivityArgs.fromIntent(intent = intent)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val args = runCatching {
+            requireNotNull(inputArgs) {
+                "AaniPayActivity input arguments were not found"
+            }
+        }.getOrElse {
+            finishWithData(CardPaymentData(CardPaymentData.STATUS_PAYMENT_FAILED))
+            return
+        }
+
+        setContent {
+            var showTimer by remember { mutableStateOf(false) }
+
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "Aani Pay",
+                                color = colorResource(id = R.color.payment_sdk_pay_button_text_color)
+                            )
+                        },
+                        backgroundColor = colorResource(id = R.color.payment_sdk_toolbar_color),
+                        navigationIcon = {
+                            IconButton(onClick = { }) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    tint = colorResource(id = R.color.payment_sdk_toolbar_icon_color),
+                                    contentDescription = "Back"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) { contentPadding ->
+                Column(
+                    modifier = Modifier.padding(contentPadding)
+                ) {
+                    if (showTimer) {
+                        AaniPayTimerScreen(args.amount)
+                    } else {
+                        AaniPayScreen {
+                            showTimer = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun finishWithData(cardPaymentData: CardPaymentData) {
+        val intent = Intent().apply {
+            putExtra(CardPaymentData.INTENT_DATA_KEY, cardPaymentData)
+        }
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}
