e4ea4164125ae4f6e70c133a6667a5545c0cfb73
Gautam <gautam.chibde@network.global>
2024-08-28 22:07:32 +0530

    NINGG-10824 - fix validation for  aani pay, added launcher for aani pay

--- app/src/test/java/payment/sdk/android/demo/MainViewModelTest.kt
+++ app/src/test/java/payment/sdk/android/demo/MainViewModelTest.kt
@@ -20,7 +20,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import payment.sdk.android.PaymentClient
-import payment.sdk.android.cardpayment.CardPaymentData
 import payment.sdk.android.core.Order
 import payment.sdk.android.core.SavedCard
 import payment.sdk.android.demo.data.DataStore
@@ -28,6 +27,8 @@ import payment.sdk.android.demo.http.CreateOrderApiInteractor
 import payment.sdk.android.demo.http.GetOrderApiInteractor
 import payment.sdk.android.demo.model.Environment
 import payment.sdk.android.demo.model.EnvironmentType
+import payment.sdk.android.demo.model.OrderRequest
+import payment.sdk.android.demo.model.PaymentOrderAmount
 import payment.sdk.android.demo.model.Product
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -114,20 +115,27 @@ class MainViewModelTest {
             orderResponse
         )
         coEvery { paymentClient.launchCardPayment(any(), any()) } returns Unit
-
-        sut.onPayByCard()
+        sut.createOrder(PaymentType.CARD, orderRequest = OrderRequest(
+            action = "SALE",
+            amount = PaymentOrderAmount(
+                value = 100.00,
+                currencyCode = "AED"
+            ),
+            language = "en",
+            merchantAttributes = emptyMap(),
+        ))
 
         assertEquals(states[0].state, MainViewModelStateType.INIT)
         assertEquals(states[1].state, MainViewModelStateType.LOADING)
         assertEquals(states[1].message, "Creating Order...")
-        assertEquals(states[2].state, MainViewModelStateType.INIT)
+        assertEquals(states[2].state, MainViewModelStateType.PAYMENT_PROCESSING)
         assertEquals(states[2].orderReference, orderResponse.reference)
         assertEquals(states[2].selectedProducts, emptyList<Product>())
         assertEquals(states[2].total, 0.0)
     }
 
     @Test
-    fun `test onPayByCard if selected environment is not set`() = runTest {
+    fun `test createOrder if selected environment is not set`() = runTest {
 
         val states: MutableList<MainViewModelState> = mutableListOf()
 
@@ -137,7 +145,15 @@ class MainViewModelTest {
 
         coEvery { dataStore.getSelectedEnvironment() } returns null
 
-        sut.onPayByCard()
+        sut.createOrder(PaymentType.CARD, orderRequest = OrderRequest(
+            action = "SALE",
+            amount = PaymentOrderAmount(
+                value = 100.00,
+                currencyCode = "AED"
+            ),
+            language = "en",
+            merchantAttributes = emptyMap(),
+        ))
 
         assertEquals(states[0].state, MainViewModelStateType.INIT)
         assertEquals(states[1].state, MainViewModelStateType.ERROR)
@@ -161,218 +177,19 @@ class MainViewModelTest {
             )
         } returns Result.Error("failed")
 
-        sut.onPayByCard()
+        sut.createOrder(PaymentType.CARD, OrderRequest(action = "SALE",
+            amount = PaymentOrderAmount(
+                value = 100.00,
+                currencyCode = "AED"
+            ),
+            language = "en",
+            merchantAttributes = emptyMap()))
 
-        assertEquals(states[0].state, MainViewModelStateType.INIT)
+        assertEquals(states[0].state, MainViewModelStateType.INIT
+        )
         assertEquals(states[1].state, MainViewModelStateType.LOADING)
         assertEquals(states[2].state, MainViewModelStateType.ERROR)
         assertEquals(states[2].message, "failed")
-    }
-
-    @Test
-    fun `test onSamsung Pay Success`() = runTest {
-        val orderResponse = Gson().fromJson(
-            ClassLoader.getSystemResource("order_response.json").readText(),
-            Order::class.java
-        )
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        coEvery { dataStore.getSelectedEnvironment() } returns environment
-        coEvery {
-            createOrderApiInteractor.createOrder(any(), any())
-        } returns Result.Success(orderResponse)
-
-        sut.onSamsungPay()
-
-        coVerify { paymentClient.launchSamsungPay(any(), any(), any()) }
-
-        assertEquals(states[0].state, MainViewModelStateType.INIT)
-        assertEquals(states[1].state, MainViewModelStateType.LOADING)
-        assertEquals(states[2].state, MainViewModelStateType.INIT)
-        assertEquals(states[2].total, 0.0)
-    }
-
-    @Test
-    fun `test onSamsung Pay failed`() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) { sut.state.toList(states) }
-
-        coEvery { dataStore.getSelectedEnvironment() } returns environment
-        coEvery {
-            createOrderApiInteractor.createOrder(any(), any())
-        } returns Result.Error("Error")
-
-        sut.onSamsungPay()
-
-        assertEquals(states[0].state, MainViewModelStateType.INIT)
-        assertEquals(states[1].state, MainViewModelStateType.LOADING)
-        assertEquals(states[2].state, MainViewModelStateType.ERROR)
-        assertEquals(states[2].message, "Error")
-    }
-
-    @Test
-    fun `show error onCardPaymentResponse if payment is successful but no environment selected`() =
-        runTest {
-            val states: MutableList<MainViewModelState> = mutableListOf()
-
-            backgroundScope.launch(testDispatcher) {
-                sut.state.toList(states)
-            }
-
-            coEvery { dataStore.getSelectedEnvironment() } returns null
-
-            sut.onCardPaymentResponse(CardPaymentData(code = CardPaymentData.STATUS_PAYMENT_CAPTURED))
-
-            assertEquals(states[1].state, MainViewModelStateType.LOADING)
-            assertEquals(states[2].state, MainViewModelStateType.ERROR)
-            assertEquals(states[2].message, "No environment selected")
-        }
-
-    @Test
-    fun `show success onCardPaymentResponse if payment is successful with environment selected`() =
-        runTest {
-            val orderResponse = Gson().fromJson(
-                ClassLoader.getSystemResource("order_response.json").readText(),
-                Order::class.java
-            )
-            val states: MutableList<MainViewModelState> = mutableListOf()
-
-            backgroundScope.launch(testDispatcher) {
-                sut.state.toList(states)
-            }
-
-            coEvery { dataStore.getSelectedEnvironment() } returns environment
-            coEvery { dataStore.getSavedCards() } returns emptyList()
-            coEvery { getOrderApiInteractor.getOrder(any(), any()) } returns Result.Success(
-                orderResponse
-            )
-
-            sut.onCardPaymentResponse(CardPaymentData(code = CardPaymentData.STATUS_PAYMENT_CAPTURED))
-
-            coVerify(exactly = 1) { dataStore.saveCard(any()) }
-            coVerify(exactly = 1) { dataStore.setSavedCard(any()) }
-
-            assertEquals(states[1].state, MainViewModelStateType.LOADING)
-            assertEquals(states[2].state, MainViewModelStateType.PAYMENT_SUCCESS)
-            assertEquals(states[2].message, "Payment Successful")
-            assertEquals(states[2].savedCards, emptyList<SavedCard>())
-        }
-
-    @Test
-    fun `show Success onCardPaymentResponse if get order returns error`() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        coEvery { dataStore.getSelectedEnvironment() } returns environment
-        coEvery { dataStore.getSavedCards() } returns emptyList()
-        coEvery { getOrderApiInteractor.getOrder(any(), any()) } returns Result.Error("error")
-
-        sut.onCardPaymentResponse(CardPaymentData(code = CardPaymentData.STATUS_PAYMENT_CAPTURED))
-
-        assertEquals(states[1].state, MainViewModelStateType.LOADING)
-        assertEquals(states[2].state, MainViewModelStateType.PAYMENT_SUCCESS)
-    }
-
-    @Test
-    fun `test onCardPaymentResponse is non success calls`() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        sut.onCardPaymentResponse(CardPaymentData(code = CardPaymentData.STATUS_PAYMENT_FAILED))
-
-        assertEquals(states.last().state, MainViewModelStateType.ERROR)
-
-
-        sut.onCardPaymentResponse(CardPaymentData(code = CardPaymentData.STATUS_POST_AUTH_REVIEW))
-
-        assertEquals(states.last().state, MainViewModelStateType.PAYMENT_POST_AUTH_REVIEW)
-
-        sut.onCardPaymentResponse(CardPaymentData(code = 100))
-
-        assertEquals(states.last().state, MainViewModelStateType.PAYMENT_FAILED)
-    }
-
-    @Test
-    fun `onSavedCardPayment successful`() = runTest {
-        val orderResponse = Gson().fromJson(
-            ClassLoader.getSystemResource("order_response.json").readText(),
-            Order::class.java
-        )
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        coEvery { createOrderApiInteractor.createOrder(any(), any()) } returns Result.Success(
-            orderResponse
-        )
-        coEvery { dataStore.getSelectedEnvironment() } returns environment
-
-        sut.onPayBySavedCard(savedCard)
-
-        assertEquals(states[1].state, MainViewModelStateType.LOADING)
-        assertEquals(states[2].state, MainViewModelStateType.INIT)
-        assertEquals(states[2].selectedProducts, emptyList<Product>())
-        assertEquals(states[2].total, 0.0)
-    }
-
-    @Test
-    fun `onSavedCardPayment environment not selected`() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        coEvery { dataStore.getSelectedEnvironment() } returns null
-
-        sut.onPayBySavedCard(savedCard)
-
-        assertEquals(states[1].state, MainViewModelStateType.ERROR)
-        assertEquals(states[1].message, "No environment selected")
-    }
-
-    @Test
-    fun `onSavedCardPayment error while creating order`() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-
-        coEvery { createOrderApiInteractor.createOrder(any(), any()) } returns Result.Error("error")
-        coEvery { dataStore.getSelectedEnvironment() } returns environment
-
-        sut.onPayBySavedCard(savedCard)
-
-        assertEquals(states[1].state, MainViewModelStateType.LOADING)
-        assertEquals(states[2].state, MainViewModelStateType.ERROR)
-        assertEquals(states[2].message, "error")
-//        assertEquals(states[2].selectedProducts, emptyList<Product>())
-//        assertEquals(states[2].total, 0.0)
-    }
-
-    @Test
-    fun onCardPaymentCancelledTest() = runTest {
-        val states: MutableList<MainViewModelState> = mutableListOf()
-
-        backgroundScope.launch(testDispatcher) {
-            sut.state.toList(states)
-        }
-        sut.onCardPaymentCancelled()
-        assertEquals(states[1].state, MainViewModelStateType.PAYMENT_CANCELLED)
     }
 
     @Test
--- payment-sdk/src/main/java/payment/sdk/android/cardpayment/validation/UppercaseVisualTransformation.kt
+++ payment-sdk/src/main/java/payment/sdk/android/cardpayment/validation/UppercaseVisualTransformation.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.toUpperCase
 
-class UppercaseVisualTransformation : VisualTransformation {
+internal class UppercaseVisualTransformation : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
         return TransformedText(text.toUpperCase(), object : OffsetMapping {
             override fun originalToTransformed(offset: Int) = offset
