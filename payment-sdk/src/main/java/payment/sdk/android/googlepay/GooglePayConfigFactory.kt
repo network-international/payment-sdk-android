07d1305a6082e819f44e4e047279afa649e2b130
Gautam <gautam.chibde@network.global>
2025-01-16 13:19:45 +0530

    NINGG-12979 - Added Jaywan scheme
    (cherry picked from commit ccfeddfef8e45562122a06bdd1c350ee0efadf93)

--- payment-sdk/src/main/java/payment/sdk/android/googlepay/GooglePayConfigFactory.kt
+++ payment-sdk/src/main/java/payment/sdk/android/googlepay/GooglePayConfigFactory.kt
@@ -1,19 +1,24 @@
 package payment.sdk.android.googlepay
 
+import android.content.Context
 import com.google.android.gms.wallet.IsReadyToPayRequest
 import com.google.android.gms.wallet.PaymentDataRequest
 import com.google.android.gms.wallet.PaymentsClient
+import com.google.android.gms.wallet.Wallet
+import com.google.android.gms.wallet.WalletConstants
 import kotlinx.coroutines.tasks.await
 import payment.sdk.android.payments.GooglePayUiConfig
 import payment.sdk.android.core.GooglePayConfigResponse
 import payment.sdk.android.core.interactor.GooglePayConfigInteractor
 
 internal class GooglePayConfigFactory(
-    private val paymentsClient: PaymentsClient,
+    private val context: Context,
     private val googlePayJsonConfig: GooglePayJsonConfig,
     private val googlePayConfigInteractor: GooglePayConfigInteractor,
     private val merchantGatewayId: String
 ) {
+    private lateinit var paymentsClient: PaymentsClient
+
     suspend fun checkGooglePayConfig(
         googlePayConfigUrl: String?,
         accessToken: String,
@@ -43,6 +48,12 @@ internal class GooglePayConfigFactory(
         currencyCode: String
     ): GooglePayUiConfig? {
         try {
+            paymentsClient = Wallet.getPaymentsClient(
+                context,
+                Wallet.WalletOptions.Builder()
+                    .setEnvironment(googlePayConfigResponse.env())
+                    .build()
+            )
             val paymentDataRequestJson = googlePayJsonConfig.create(
                 googlePayConfigResponse = googlePayConfigResponse,
                 amount = amount,
@@ -82,4 +93,10 @@ internal class GooglePayConfigFactory(
     companion object {
         private const val KEY_GOOGLE_PAY = "GOOGLE_PAY"
     }
+}
+
+private fun GooglePayConfigResponse.env() = if (environment == "PRODUCTION") {
+    WalletConstants.ENVIRONMENT_PRODUCTION
+} else {
+    WalletConstants.ENVIRONMENT_TEST
 }
